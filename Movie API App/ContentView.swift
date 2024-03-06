308df275f5606cd0fd10302953706059b232a94b
fadhli <fadliprestigious@gmail.com>
2024-03-06 19:58:08 +0800

    Initial Commit

---
+++ Movie API App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movie API App
+//
+//  Created by Fadhli Firdaus on 06/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Movie API App/Movie_API_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_API_AppApp.swift
+//  Movie API App
+//
+//  Created by Fadhli Firdaus on 06/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Movie_API_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
